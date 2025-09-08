da1b07bbf033470a0779525f7686a9a45818e205
Sai Krishna <dsaikrishna784@gmail.com>
2025-09-08 14:31:38 +0530

    Update test_app.py

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,10 +1,13 @@
-import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import home, health
 
 def test_home():
-    assert app.home() == "Hello from CI/CD Pipeline!"
+    assert home() == "Hello from CI/CD Pipeline!"
 
 def test_health():
-    response = app.health()
+    response = health()
     assert response[0] == "OK"
     assert response[1] == 200
-
